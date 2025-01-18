93c9965d2bf42ad7fa5be9a5f32ada4d6b138a8e
Solange883 <soldesages88@gmail.com>
2025-01-18 18:45:34 +0000

    Changement

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
             <span className="text-secondary md:ml-7">Marie Solange Diouf</span>
             </h1>
 
-            <p className="my-4 text-xl text-center md:text-left md:ml-9">Développeur Full Stack</p>
+            <p className="my-4 text-xl text-center md:text-left md:ml-9">Développeur Full Stac</p>
 
            <div className="flex flew-row justify-center">
             <a href="mailto:soldesanges88@gmail.com" className="my-5 btn btn-secondary md:w-fit  ">
