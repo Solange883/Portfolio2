496c921579a1e99f5604dd6b512ed8f4b5d27536
Solange883 <soldesages88@gmail.com>
2025-02-23 14:11:45 +0000

    changement

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Portfolio2/",
+  // eslint-disable-next-line no-undef
+  base: process.env.VERCEL ? "/" : "/Portfolio2/", // "/" pour Vercel, "/Portfolio2/" pour GitHub Pages
   plugins: [react()],
  
  
